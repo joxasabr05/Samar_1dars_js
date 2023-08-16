9b86d2496965f61f532ab1973e1bc3c09e1909f4
joxasabr05 <jaxongirmirhalikov@gmail.com>
2023-08-15 22:51:12 -0700

    New Year

--- js/js.js
+++ js/js.js
@@ -4,7 +4,9 @@ const person = {
 console.log(person);
 person.name = "Fozil";
 console.log(person);
-
+let number = 10;
+number = 20;
+console.log(number);
 // Old version
 console.log(className);
 var className = "Zebo";
