d41587f7a11232a75b2e2a87f5ad490cd9610b8e
joxasabr05 <jaxongirmirhalikov@gmail.com>
2023-08-15 21:42:06 -0700

    join

--- js/js.js
+++ js/js.js
@@ -1,2 +1,10 @@
-let number = 10
-console.log(number);
+const person = {
+  name: "Jaxongir",
+};
+console.log(person);
+person.name = "Fozil";
+console.log(person);
+
+// Old version
+console.log(className);
+var className = "Zebo";
